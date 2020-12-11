00a0e55dfa96bca56471c2beeffd81a0f350d9a6
Dennis <dmarlo96@live.ca>
2020-12-11 17:08:48 -0500

    Added Coupon with Crud Ops

---
+++ Restaurant/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Data;
+using Restaurant.Models;
+
+namespace Restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var coupons = await _db.Coupon.ToListAsync();
+
+            return View(coupons);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupon)
+        {
+            if (ModelState.IsValid)
+            {
+                // if it is valid we'll fetch the file that was uploaded for the image 
+                var files = HttpContext.Request.Form.Files;
+
+                // file was uploaded
+                if (files.Count > 0)
+                {
+                    //  we need to convert the file into a stream of bytes to store it into a database
+                    byte[] p1 = null;
+
+                    // this will start reading the file
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        // So what this will do is it will convert our image into a stream of bytes and stored it into var p1
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+
+                    }
+                    //  and then we just need to add it to our pictures inside the database.
+                    coupon.Picture = p1;
+                }
+                // add all the other entries to the database
+                _db.Coupon.Add(coupon);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupon);
+        }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Coupon coupon)
+        {
+            if (coupon.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await _db.Coupon.Where(c => c.Id == coupon.Id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    couponFromDb.Picture = p1;
+                }
+                couponFromDb.MinimumAmount = coupon.MinimumAmount;
+                couponFromDb.Name = coupon.Name;
+                couponFromDb.Discount = coupon.Discount;
+                couponFromDb.CouponType = coupon.CouponType;
+                couponFromDb.IsActive = coupon.IsActive;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupon);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coupons = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Coupon.Remove(coupons);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
